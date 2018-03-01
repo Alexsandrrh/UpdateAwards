bc71225b51c692dfc20a7689cd7f788d7a6a54a5
Alexsandrrh <sadovspbsasha@outlook.com>
2018-03-01 18:53:57 +0300

    update pages

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+    let backgrounds = [
+        'bg-main1.jpg',
+        'bg-main2.jpg',
+        'bg-main3.jpg',
+        'bg-main4.jpg'
+    ]
+
+    $('#particles-js').css({
+        'background-image': 'url(assets/img/' + backgrounds [Math.floor(Math.random()  * (4 - 0) + 0)] + ')'
+    })
 	$('.nav-toggle').click(function () {
         $('.mobile-nav').toggleClass('-open');
     })
@@ -8,6 +18,5 @@ $(document).ready(function() {
         $('.section-main h1').addClass('-animated');
         $('.section-main h2').addClass('-animated');
 
-    }, 500);
-
+    }, 2000);
 });
